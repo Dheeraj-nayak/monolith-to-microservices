9b2e8bedb503dce7cb880aaa5f4b643a949d67cd
dheerajn <dheerajn@andrew.cmu.com>
2023-12-10 16:30:12 -0500

    updated frontend tests

--- microservices/src/frontend/__tests__/unit_tests.js
+++ microservices/src/frontend/__tests__/unit_tests.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const app = require('../server'); // Adjust the path as necessary
-
 let server;
 
 beforeAll((done) => {
@@ -11,33 +10,25 @@ afterAll((done) => {
   server.close(done);
 });
 
-it('should start server', () => {
-  expect(server.listening).toBe(true);
-});
-
-describe('Static File Serving', () => {
-  it('should serve static files', async () => {
+describe('Frontend Service Tests', () => {
+  it('should serve the main page', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.headers['content-type']).toContain('text/html');
+    expect(res.text).toContain('<div id="root"></div>'); // Check for a specific part of your main page
+  });
+
+  it('should handle non-existent routes', async () => {
+    const res = await request(app).get('/non-existent-route');
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toContain('text/html');
+    expect(res.text).toContain('<div id="root"></div>'); // The SPA's entry point should still be served
+  });
+
+  it('should serve static assets', async () => {
+    // Assuming there's a static image at public/static/img/sample.jpg
+    const res = await request(app).get('/static/img/camera-lens.jpg');
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toContain('image/jpeg');
   });
 });
-
-
-describe('Client-Side Routing', () => {
-    it('should handle non-root URLs', async () => {
-      const res = await request(app).get('/some-non-existent-route');
-      expect(res.statusCode).toEqual(200);
-      expect(res.headers['content-type']).toContain('text/html');
-    });
-  });
-
-  
-
-describe('Server Startup', () => {
-    it('should start server on specified port', () => {
-      const port = process.env.PORT || 8080;
-      expect(app.listen).toHaveBeenCalledWith(port, expect.any(Function));
-    });
-  });
-  
