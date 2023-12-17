4691f984e0138dde190f83cd0283c23544240914
dheerajn <dheerajn@andrew.cmu.com>
2023-12-17 18:54:04 -0500

    added selenium test - yet to modify URL

---
+++ microservices/e2e_tests/selenium_test.js
@@ -0,0 +1,34 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+async function testMicroservicesApp() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        // Navigate to the Home page
+        await driver.get('http:/10.0.0.4:8080/');
+        await driver.wait(until.titleIs('Your Page Title'), 10000); 
+
+        // Validate Home Page
+        let homeText = await driver.findElement(By.css('Typography')).getText();
+        console.assert(homeText.includes('Welcome to the Fancy Store!'), 'Home page text not found');
+
+        // Navigate to the Orders page
+        await driver.findElement(By.linkText('Orders')).click(); // Replace with actual text or identifier
+        await driver.wait(until.elementLocated(By.css('Table')), 10000); // Wait for the table to load
+
+        // Validate Orders Page
+        let ordersTable = await driver.findElement(By.css('Table'));
+        console.assert(ordersTable.isDisplayed(), 'Orders table not displayed');
+
+        // Navigate to the Products page
+        await driver.findElement(By.linkText('Products')).click(); // Replace with actual text or identifier
+        await driver.wait(until.elementLocated(By.css('Grid')), 10000); // Wait for the grid to load
+
+        // Validate Products Page
+        let productsGrid = await driver.findElement(By.css('Grid'));
+        console.assert(productsGrid.isDisplayed(), 'Products grid not displayed');
+
+    } finally {
+        await driver.quit();
+    }
+}
+testMicroservicesApp();
