89a72471d1cb9903b949472ba062aecd780cf5ca
dheerajn <dheerajn@andrew.cmu.com>
2023-12-18 15:12:42 -0500

    added debug statements

--- microservices/e2e_tests/selenium_test.js
+++ microservices/e2e_tests/selenium_test.js
@@ -1,3 +1,5 @@
+console.log("Starting test...");
+
 const { Builder, By, Key, until, Capabilities } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
@@ -6,13 +8,18 @@ async function testMicroservicesApp() {
     options.addArguments('--headless');
     options.addArguments('--no-sandbox');
     options.addArguments('--disable-dev-shm-usage');
+    options.addArguments('--disable-gpu'); // Disable GPU hardware acceleration
+    options.addArguments('--window-size=1920,1080'); // Specify window size
+
 
     let driver = await new Builder()
         .forBrowser('chrome')
         .setChromeOptions(options)
         .build();
 
+
     try {
+        console.log("Navigating to the Home page...");
          // Navigate to the Home page
          await driver.get('http://10.0.0.4:8080');
          //await driver.wait(until.titleIs('Your Page Title'), 10000); 
@@ -22,10 +29,12 @@ async function testMicroservicesApp() {
          console.assert(homeText.includes('Welcome to the Fancy Store!'), 'Home page text not found');
  
          // Navigate to the Orders page
+         console.log("Navigating to the Orders page...");
          await driver.findElement(By.linkText('Orders')).click(); // Replace with actual text or identifier
          await driver.wait(until.elementLocated(By.css('Table')), 10000); // Wait for the table to load
  
          // Validate Orders Page
+         console.log("Navigating to the Products page...");
          let ordersTable = await driver.findElement(By.css('Table'));
          console.assert(ordersTable.isDisplayed(), 'Orders table not displayed');
  
@@ -37,7 +46,10 @@ async function testMicroservicesApp() {
          let productsGrid = await driver.findElement(By.css('Grid'));
          console.assert(productsGrid.isDisplayed(), 'Products grid not displayed');
  
-    } finally {
+    }catch (error) {
+        console.error("Error during test execution:", error); 
+    }finally {
+        console.log("Closing the browser...");
         await driver.quit();
     }
 }
